b300753461ebd367950832196e8578f530627144
Stella Ndjikessi <stelladiana@live.fr>
2023-11-14 13:56:07 +0100

    Rail Task Manager methods done, style is so so

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,20 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+  # Read all
+  # get "tasks", to: "tasks#index", as: :tasks
+
+  # # Create task
+  # get "tasks/new", to: "tasks#new", as: :new_task
+  # post "tasks", to: "tasks#create"
+  # # Read one
+  # get "tasks/:id", to: "tasks#show", as: :task
+
+  # # Update task
+  # get "tasks/:id/edit", to: "tasks#edit", as: :edit_task
+  # patch "tasks/:id", to: "tasks#update"
+
+  # # Delete task
+  # delete "tasks/:id", to: "tasks#destroy"
+  resources :tasks
 end
